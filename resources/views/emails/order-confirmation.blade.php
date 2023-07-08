0b2553910f60818c86aff8c67cccfe82db73182f
vanhuytran2610 <vanhuytran2610@gmail.com>
2023-07-08 18:39:16 +0700

    Last update

--- resources/views/emails/order-confirmation.blade.php
+++ resources/views/emails/order-confirmation.blade.php
@@ -6,21 +6,21 @@
 <body>
     <h1>Order Confirmation</h1>
     
-    <p>Dear {{ $order->firstname }} {{ $order->lastname }},</p>
+    <p style="font-weight: 600;">Dear {{ $order->firstname }} {{ $order->lastname }},</p>
     
-    <p>Thank you for placing an order with us. Your order has been successfully confirmed.</p>
+    <p style="font-weight: 600;">Thank you for placing an order with us. Your order has been successfully confirmed.</p>
 
-    <h5>Payment Methods: Pay after receive</h5>
+    <h5>Payment Methods: Payment on delivery</h5>
     
     <h2>Order Details:</h2>
     
-    <p>Order Date: {{ $order->created_at->format('d-m-Y H:i:s') }}</p>
+    <p style="font-weight: 600;">Order Date: {{ $order->created_at->format('d-m-Y H:i:s') }}</p>
     <!-- Include any other relevant order details -->
-    <p>Link: http://localhost:3000/order-list</p>
+    <p style="font-weight: 600;">Link: http://localhost:3000/order-list</p>
     
-    <p>If you have any questions or need further assistance, please don't hesitate to contact us.</p>
+    <p style="font-weight: 600;">If you have any questions or need further assistance, please don't hesitate to contact us.</p>
     
-    <p>Best regards,</p>
-    <p>H Store customer service.</p>
+    <p style="font-weight: 600;">Best regards,</p>
+    <p style="font-weight: 600;">H Store customer service.</p>
 </body>
 </html>
